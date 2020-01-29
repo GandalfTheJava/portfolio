69829d5d2d1cafb35216a7a4ca9cb24acaffcc77
mehmet <mehmetcimen23@gmail.com>
2020-01-29 15:28:13 +0000

    Initial project

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ app.post('/', function (req, res) {
     res.redirect("/");
 });
 
-app.listen(3000, function (err) {
+app.listen(process.env.PORT || 3000, function (err) {
     if (err) {
         console.log(err);
     } else {
