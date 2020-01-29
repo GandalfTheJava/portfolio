d3624b9e35deb3fe2f60d986302886fe75af614c
mehmet <mehmetcimen23@gmail.com>
2020-01-29 15:40:03 +0000

    Refactoring

--- app.js
+++ app.js
@@ -16,15 +16,13 @@ app.post('/', function (req, res) {
     // async..await is not allowed in global scope, must use a wrapper
     async function main() {
         // Generate test SMTP service account from ethereal.email
-        // Only needed if you don't have a real mail account for testing
-        let testAccount = await nodemailer.createTestAccount();
 
         // create reusable transporter object using the default SMTP transport
         let transporter = nodemailer.createTransport({
             service: "Gmail",
             auth: {
-                user: process.env.GMAIL_USER, // generated ethereal user
-                pass: process.env.GMAIL_PASS // generated ethereal password
+                user: process.env.GMAIL_USER,
+                pass: process.env.GMAIL_PASS
             }
         });
 
